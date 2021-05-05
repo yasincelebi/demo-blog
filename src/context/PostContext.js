48b7012a53b39c195edfd8dae098ae465a77c4bf
yasincelebi <yasincelebix@gmail.com>
2021-05-05 17:54:03 +0300

    Add totalpost

---
+++ src/context/PostContext.js
@@ -0,0 +1,20 @@
+import React, { createContext, useEffect, useState } from "react";
+
+import BlogData from "../data/data.json";
+export const PostContext = createContext();
+export const PostProvider = (props) => {
+  const [postsData, setPostsData] = useState([]);
+  const [totalPost, setTotalPost] = useState("");
+  const fetchData = () => {
+    setTotalPost(BlogData["posts-total"]);
+    setPostsData(JSON.parse(JSON.stringify(BlogData.posts)));
+  };
+  useEffect(() => {
+    fetchData();
+  });
+  return (
+    <PostContext.Provider value={{ postsData, setPostsData, totalPost }}>
+      {props.children}
+    </PostContext.Provider>
+  );
+};
